eb456d62cf4d6abd3f140e35ff4950d788f25954
Lebogang <lebogangodeveloper@gmail.com>
2023-06-22 15:36:19 +0200

    Retrieve instruction for a given opcode

--- get_instruction.c
+++ get_instruction.c
@@ -1,5 +1,3 @@
-#include <string.h>
-#include <stdlib.h>
 #include "monty.h"
 
 /**
@@ -20,59 +18,89 @@ char *strdup(const char *str)
 }
 
 /**
+ * create_instruction - Creates an instruction_t struct.
+ * @opcode: The opcode of the instruction.
+ * @f: The function pointer of the instruction.
+ *
+ * Return: Pointer to the created instruction_t struct.
+ */
+
+instruction_t *create_instruction(char *opcode, void (*f)
+		(stack_t **stack, unsigned int line_number))
+{
+	instruction_t *instruction = malloc(sizeof(instruction_t));
+
+	if (!instruction)
+		return (NULL);
+
+	instruction->opcode = strdup(opcode);
+	instruction->f = f;
+	return (instruction);
+}
+
+/**
+ * free_instructions - Frees the memory of an array of instruction_t structs.
+ * @instructions: The array of instruction_t structs.
+ */
+
+void free_instructions(instruction_t *instructions)
+{
+	int i;
+
+	if (instructions == NULL)
+		return;
+
+	for (i = 0; instructions[i].opcode; i++)
+	{
+		free(instructions[i].opcode);
+	}
+
+	free(instructions);
+}
+
+/**
  * get_instruction - Retrieves the corresponding instruction
  * for a given opcode.
  * @opcode: The opcode to search for.
  *
- * Return: A pointer to the instruction_t struct that matches the given
- * opcode,or NULL if no match is found.
+ * Return: A pointer to the instruction_t struct.
  */
 
 instruction_t *get_instruction(char *opcode)
 {
-	int index = 0;
+	int index;
 
 	instruction_t *instructions = malloc(sizeof(instruction_t) * 8);
 
 	if (!instructions)
 		return (NULL);
 
-	instructions[0].opcode = "push";
-	instructions[0].f = push;
-	instructions[1].opcode = "pall";
-	instructions[1].f = pall;
-	instructions[2].opcode = "pint";
-	instructions[2].f = pint;
-	instructions[3].opcode = "pop";
-	instructions[3].f = pop;
-	instructions[4].opcode = "swap";
-	instructions[4].f = swap;
-	instructions[5].opcode = "add";
-	instructions[5].f = add;
-	instructions[6].opcode = "nop";
-	instructions[6].f = nop;
+	instructions[0] = *create_instruction("push", push);
+	instructions[1] = *create_instruction("pall", pall);
+	instructions[2] = *create_instruction("pint", pint);
+	instructions[3] = *create_instruction("pop", pop);
+	instructions[4] = *create_instruction("swap", swap);
+	instructions[5] = *create_instruction("add", add);
+	instructions[6] = *create_instruction("nop", nop);
 	instructions[7].opcode = NULL;
 	instructions[7].f = NULL;
+
+	index = 0;
 
 	while (instructions[index].opcode)
 	{
 		if (strcmp(instructions[index].opcode, opcode) == 0)
 		{
-			instruction_t *instruction =
-				malloc(sizeof(instruction_t));
-			if (!instruction)
-			{
-				free(instructions);
-				return (NULL);
-			}
-			instruction->opcode =
-				strdup(instructions[index].opcode);
-			instruction->f = instructions[index].f;
-			free(instructions);
+			instruction_t *instruction = create_instruction
+				(instructions[index].opcode,
+				 instructions[index].f);
+			free_instructions(instructions);
 			return (instruction);
 		}
+
 		index++;
 	}
-	free(instructions);
+
+	free_instructions(instructions);
 	return (NULL);
 }
