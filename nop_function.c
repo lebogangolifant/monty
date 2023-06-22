53c568f576ded65e7c5c5cda0a1c71d9abd87a0f
Lebogang <lebogangodeveloper@gmail.com>
2023-06-22 13:17:33 +0200

    Implement the nop opcode.

---
+++ nop_function.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+
+/**
+ * nop - Doesn't do anything.
+ * @stack: Double pointer to the stack.
+ * @line_number: Line number of the instruction.
+ */
+
+void nop(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+}
